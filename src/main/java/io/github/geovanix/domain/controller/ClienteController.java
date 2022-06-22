6421b746d5d0e1f14299393d2527dc2631ed7203
Geovani Xavier <geovani.santos@g4f.com.br>
2022-06-22 00:04:08 -0300

    Refatoração do ClienteController com base nos conhecimentos de Java 8.

--- src/main/java/io/github/geovanix/domain/controller/ClienteController.java
+++ src/main/java/io/github/geovanix/domain/controller/ClienteController.java
@@ -5,75 +5,80 @@ import io.github.geovanix.domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
+/*
+Ao Usar RestController ao invés de controller ele já vem com a annotation @ResponseBody Embutida,
+Ou seja, não precisa colocar @ResponseBody em cima de cada metodo.
+ */
 @RestController
-@RequestMapping("/")
+
+//Ao usar o @RequestMapping, usar uma url universal e se necessario ir passando os parametros como por exemplo: {id}
+@RequestMapping("/api/cliente/")
 public class ClienteController {
 
     @Autowired
     private Clientes clientes;
 
     //BuscarPorId
-    @GetMapping("/api/cliente/{id}")
-    @ResponseBody
-    public ResponseEntity getClienteById(@PathVariable Integer id) {
-        Optional<Cliente> cliente = clientes.findById(id);
-        if (cliente.isPresent()) {
-            return ResponseEntity.ok(cliente.get());
-        }
-        return ResponseEntity.notFound().build();
+    @GetMapping("{id}")
+    public Cliente getClienteById(@PathVariable Integer id) {
+        return clientes.findById(id)
+                       .orElseThrow(() ->
+                       new ResponseStatusException(HttpStatus.NOT_FOUND,
+                       "Cliente não encontrando."));
+
     }
 
     //Metodo Salvar
-    @PostMapping("/api/clientes")
-    @ResponseBody
-    public ResponseEntity save(@RequestBody Cliente cliente) {
-        Cliente clienteSalvo = clientes.save(cliente);
-        return ResponseEntity.ok(clienteSalvo);
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente save(@RequestBody Cliente cliente) {
+        return clientes.save(cliente);
     }
 
 
     //Metodo Deletar
-    @DeleteMapping("/api/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity delete(@PathVariable Integer id) {
-        Optional<Cliente> cliente = clientes.findById(id);
-        if (cliente.isPresent()) {
-            clientes.delete(cliente.get());
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        clientes.findById(id)
+                .map(cliente -> {
+                    clientes.delete(cliente);
+                    return cliente;
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Cliente não encontrado"));
     }
 
     //Metodo de Atualizar
-    @PutMapping("/api/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity atualizar(@PathVariable Integer id,
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id,
                                     @RequestBody Cliente cliente) {
-        return clientes.findById(id).map(clienteExistente -> {
+         clientes.findById(id).map(clienteExistente -> {
             cliente.setId(clienteExistente.getId());
             clientes.save(cliente);
-            return ResponseEntity.noContent().build();
-        }).orElseGet( () -> ResponseEntity.notFound().build() );
+            return clienteExistente;
+        }).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+           "Cliente não encontrado"));
     }
 
     //Fazer Pesquisa por filtros com atualizações do Java8.
-    @GetMapping("/api/clientes")
-    @ResponseBody
-    public ResponseEntity find (Cliente filtro){
+    @GetMapping
+    public List<Cliente> find (Cliente filtro){
         ExampleMatcher matcher = ExampleMatcher
                                     .matching()
                                     .withIgnoreCase()
                                     .withStringMatcher(
-                                         ExampleMatcher.StringMatcher.CONTAINING);
+                                     ExampleMatcher.StringMatcher.CONTAINING);
         Example example = Example.of(filtro, matcher);
-        List<Cliente> lista = clientes.findAll(example);
-        return ResponseEntity.ok(lista);
+        return clientes.findAll(example);
+
     }
 
 }
