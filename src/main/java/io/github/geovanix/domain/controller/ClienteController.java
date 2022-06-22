b3f0c4bdfea44464f80d0687243be2c69beacee1
Geovani Xavier <geovani.santos@g4f.com.br>
2022-06-21 23:22:46 -0300

    Classe ClienteController Finalizada.

--- src/main/java/io/github/geovanix/domain/controller/ClienteController.java
+++ src/main/java/io/github/geovanix/domain/controller/ClienteController.java
@@ -3,9 +3,12 @@ package io.github.geovanix.domain.controller;
 import io.github.geovanix.domain.entity.Cliente;
 import io.github.geovanix.domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -15,9 +18,10 @@ public class ClienteController {
     @Autowired
     private Clientes clientes;
 
+    //BuscarPorId
     @GetMapping("/api/cliente/{id}")
     @ResponseBody
-    public ResponseEntity getClienteById ( @PathVariable Integer id) {
+    public ResponseEntity getClienteById(@PathVariable Integer id) {
         Optional<Cliente> cliente = clientes.findById(id);
         if (cliente.isPresent()) {
             return ResponseEntity.ok(cliente.get());
@@ -25,11 +29,51 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
+    //Metodo Salvar
     @PostMapping("/api/clientes")
     @ResponseBody
-    public ResponseEntity save( @RequestBody Cliente cliente ){
+    public ResponseEntity save(@RequestBody Cliente cliente) {
         Cliente clienteSalvo = clientes.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
+    }
+
+
+    //Metodo Deletar
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+        if (cliente.isPresent()) {
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    //Metodo de Atualizar
+    @PutMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity atualizar(@PathVariable Integer id,
+                                    @RequestBody Cliente cliente) {
+        return clientes.findById(id).map(clienteExistente -> {
+            cliente.setId(clienteExistente.getId());
+            clientes.save(cliente);
+            return ResponseEntity.noContent().build();
+        }).orElseGet( () -> ResponseEntity.notFound().build() );
+    }
+
+    //Fazer Pesquisa por filtros com atualizações do Java8.
+    @GetMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity find (Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                                    .matching()
+                                    .withIgnoreCase()
+                                    .withStringMatcher(
+                                         ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
     }
 
 }
