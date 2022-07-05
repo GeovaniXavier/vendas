3e19621e37590e07b545b775b0aaf20513ae6fc1
Geovani Xavier <geovani.santos@g4f.com.br>
2022-07-04 22:54:37 -0300

    Adicionando Annotations customizadas.

---
+++ src/main/java/io/github/geovanix/validation/NotEmptyList.java
@@ -0,0 +1,17 @@
+package io.github.geovanix.validation;
+
+import io.github.geovanix.validation.constraintValidator.NotEmptyListValidator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Constraint(validatedBy = NotEmptyListValidator.class)
+public @interface NotEmptyList {
+
+    String message() default "A lista não pode ser vazia.";
+}
