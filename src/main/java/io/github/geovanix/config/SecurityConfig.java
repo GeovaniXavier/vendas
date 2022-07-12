1e345b5514bcd6280ad82e85ca529d34b3d7be08
Geovani Xavier <geovani.santos@g4f.com.br>
2022-07-11 21:46:31 -0300

    Adicionado security ao projeto.

---
+++ src/main/java/io/github/geovanix/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package io.github.geovanix.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder passwordEncoder = new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence + "321";
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return (charSequence + "321").equals(s);
+            }
+        }
+    }
+
+    @Override
+    protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
